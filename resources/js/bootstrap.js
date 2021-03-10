7d1ea9b69d149ecc3c2d40fff7118b7d788a3b10
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-03-10 14:39:23 +0900

    fix for websocket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,9 +37,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    weHost: '127.0.0.1',
+    wsHost: '127.0.0.1',
     wsPort: 6001,
     encrypted: false,
-    // forceTLS: true
+    forceTLS: false,
     disableStats: true
 });
