f77892e2981adaf8e7967a019813a325da4bcd17
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-03-10 12:14:35 +0900

    fix broadcast for websocket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,5 +37,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    weHost: '127.0.0.1',
+    wsPort: 6001,
+    encrypted: false,
+    // forceTLS: true
+    disableStats: true
 });
